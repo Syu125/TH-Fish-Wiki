6ecb5c0c64bc61ac3d9fa3c7090ebbe50760a732
Brian Lu <brianlu09@gmail.com>
2022-02-20 19:21:09 -0800

    final changes

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ def read_root():
     #change url to "/home/index.html" after part 2
     return RedirectResponse(url="/home/part2.html")
 
-@app.get("/hello")
-def read_root():
-    # Return hardcoded image URLs
-    arr = ['https://www.fishwatch.gov/sites/default/files/white%20hake_Calvin%20Alexander_1.jpg', 
-        'https://www.fishwatch.gov/sites/default/files/Atlantic%20Chub%20Mackerel_Alessandro%20Duci.jpg', 
-        'https://www.fishwatch.gov/sites/default/files/1%20photo-west-coast-region-photo-gallery.jpg',
-        'https://www.fishwatch.gov/sites/default/files/atlantic-herring_03.jpg']
-    return arr
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5000, log_level="info")
--- solutions_part2/p2_main.py
+++
@@ -1,8 +0,0 @@
-@app.get("/hello")
-def read_root():
-    # Return hello world string
-    arr = ['https://www.fishwatch.gov/sites/default/files/white%20hake_Calvin%20Alexander_1.jpg', 
-        'https://www.fishwatch.gov/sites/default/files/Atlantic%20Chub%20Mackerel_Alessandro%20Duci.jpg', 
-        'https://www.fishwatch.gov/sites/default/files/1%20photo-west-coast-region-photo-gallery.jpg',
-        'https://www.fishwatch.gov/sites/default/files/atlantic-herring_03.jpg']
-    return arr
